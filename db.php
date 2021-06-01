15ac92c03041800f1c1853581d09ef246c76b25f
Saskia <saskia_west1996@hotmail.co.uk>
2021-06-01 18:42:02 +0100

    modifed index file and reworking db query in separate db php file

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+$dbServername = "db";
+$dbUsername = "root";
+$dbPassword = "password";
+$dbName = "supercarcollectorapp";
+
+$supercarDB = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
--- index.php
+++ index.php
@@ -1,29 +1,19 @@
 <?php
-function getSupercarDB(): PDO
-{
-   $supercarDB = new PDO('mysql:host=db; dbname=supercarcollectorapp', 'root', 'password');
-   $supercarDB->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-   return $supercarDB;
-}
+include_once 'includes/db.php';
+?>
+
+
+
 
 
 /* function getSupercars($supercarDB): array
-{
+{*/
     $query = $supercarDB->prepare('SELECT * FROM `supercars`');
     $query->execute();
     return $supercars = $query->fetchAll();
-}
 
-$supercarDB = getSupercarDB();
 
-function displaySuperCar($supercars)
-{
-    echo '<ul>';
-    foreach($supercars as $item) {
-        echo '<li>' . $item['model'] . '</li>';
-    }
-    echo '</ul>';
-}
-displaySuperCar(); */
+
+
 
 
