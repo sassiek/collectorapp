c7d1182686803f46d0204bf20a198cf37990e090
Saskia <saskia_west1996@hotmail.co.uk>
2021-06-02 08:44:20 +0100

    basic css started and normalize file added

--- index.php
+++ index.php
@@ -5,15 +5,26 @@ $db = getSupercarData();
 ?>
 
 <!DOCTYPE html>
+<html>
 <head>
     <title></title>
+    <link href="normalize.css" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <?php
 echo displaySupercarData($db);
 ?>
-
-
+<!--
+<div class="section">
+    <div class="supercarSection">
+        <p class="information"></p>
+        <p class="information"></p>
+        <p class="information"></p>
+        <p class="information"></p>
+    </div>
+</div> -->
 
 </body>
 </html>
