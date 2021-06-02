2c5b135c5c67637fd40519d9915e81cf635d3fee
Saskia <saskia_west1996@hotmail.co.uk>
2021-06-02 17:18:48 +0100

    css updated

--- index.php
+++ index.php
@@ -9,9 +9,11 @@ $db = getSupercarData();
     <title></title>
     <link href="normalize.css" rel="stylesheet" type="text/css">
     <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+<h1>Supercar Collection</h1>
 <?php
 echo displaySupercarData($db);
 ?>
